10c6c685dfef4b6884b4a18541a153c6435dbb09
Yuhao (David) Ding <1dingdav1@gmail.com>
2019-09-14 11:22:53 -0400

    First commit, kinda nervous

---
+++ main.py
@@ -0,0 +1,66 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+from google.cloud import texttospeech
+
+
+
+# Instantiates a client
+# client = speech.SpeechClient()
+credential_path = "cred.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Set the text input to be synthesized
+synthesis_input = texttospeech.types.SynthesisInput(text="Hello, World!")
+
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.types.VoiceSelectionParams(
+    language_code='en-US',
+    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+# Select the type of audio file you want returned
+audio_config = texttospeech.types.AudioConfig(
+    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+# Perform the text-to-speech request on the text input with the selected
+# voice parameters and audio file type
+response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+# The response's audio_content is binary.
+with open('output.mp3', 'wb') as out:
+    # Write the response to the output file.
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
+
+
+'''# The name of the audio file to transcribe
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources',
+    'audio.raw')
+
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code='en-US')
+
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))'''
