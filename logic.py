1a5f63b18a62a229100c8ef0f05ba6a8b8455b9d
Willson Wang <40642117+wangwillson1@users.noreply.github.com>
2019-09-15 02:27:35 -0400

    added logic

--- logic.py
+++ logic.py
@@ -1,2 +1,20 @@
-def startWord(arr)
-    
+
+
+def start_word(arr)
+    for element in arr:
+        if element == "print":
+            print_func()
+        elif element == "create":
+            create_func()
+        elif element == "loop":
+            loop_func()
+        elif element == "condition":
+            condition_func()
+        elif element == "function":
+            function_func()
+        elif element == "edit":
+            edit_func()
+        elif element == "initialize":
+            initialize_func()
+        elif element == "equals":
+            equals_func()
